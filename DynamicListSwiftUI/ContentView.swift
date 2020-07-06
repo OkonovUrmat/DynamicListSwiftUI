4dd55b68f7a5279901c2bc5395d6ca913590f0a2
OkonovUrmat <okonov.okonov@gmail.com>
2020-07-06 20:26:41 +0600

    First way to show up data in your list

--- DynamicListSwiftUI/ContentView.swift
+++ DynamicListSwiftUI/ContentView.swift
@@ -8,13 +8,25 @@
 
 import SwiftUI
 
+struct User: Identifiable {
+    var id: Int
+    
+    let username, message: String
+    
+}
+
 struct ContentView: View {
+    
+    let users: [User] = [
+        .init(id: 0, username: "Git branch", message: "Don't forse pushing your stupid ass commits"),
+        .init(id: 1,username: "Pull request", message: "I know you are stupid, but try to describe your problem more intelligibly"),
+        .init(id: 2,username: "Comit", message: "Can you explain your commit one week later? If not I'm gonna make your mum not sleep this night")
+    ]
+    
     var body: some View {
         NavigationView {
-            List {
-                Text("First Row")
-                Text("Second Row")
-                Text("Third Row")
+            List (users){
+                Text($0.username)
             }.navigationBarTitle(Text("Dynamic List"))
         }
     }
