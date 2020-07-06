254f6d40f1b11df3673cc0485a3551d42a53b183
OkonovUrmat <okonov.okonov@gmail.com>
2020-07-06 20:08:20 +0600

    Initial Commit

---
+++ DynamicListSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DynamicListSwiftUI
+//
+//  Created by Urmat on 7/6/20.
+//  Copyright © 2020 Urmat. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
