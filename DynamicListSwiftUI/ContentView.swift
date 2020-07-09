16e736683b240a55cd921f19fdd02263b5b7c26c
OkonovUrmat <okonov.okonov@gmail.com>
2020-07-09 17:18:23 +0600

    Refactor code

--- DynamicListSwiftUI/ContentView.swift
+++ DynamicListSwiftUI/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View {
         .init(id: 1,username: "Pull request", message: "I know you are stupid, but try to describe your problem more intelligibly"),
         .init(id: 2,username: "Comit", message: "Can you explain your commit one week later? If not I'm gonna make your mum not sleep this night"),
         .init(id: 3,username: "Issue", message: "When your going to write issue just fucking imagine that you are Stephen King or something, just be more not gay, OK?"),
-            .init(id: 3,username: "How to be a gay?", message: "So just make stupid commits and fuck brain of seniors, but you also can suck dick of you bosses and try it 'antoher' way. You know, huh?")
+        .init(id: 3,username: "How to be a gay?", message: "So just make stupid commits and fuck brain of seniors, but you also can suck dick of you bosses and try it 'antoher' way. You know, huh?")
     ]
     
     var body: some View {
